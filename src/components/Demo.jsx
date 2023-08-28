4dbb6b86ee85842d3bf7f8708fe32ecd53d694ec
Arzel John Zolina <80521619+ArzelJrz10@users.noreply.github.com>
2023-08-28 21:37:06 +0800

    ahhh hayst

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { copy, linkIcon, loader, tick } from '../assets';
+import React from 'react';
+import { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
-  return (
-    <div>Demo</div>
-  )
-}
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
-export default Demo
+  const handleSumbit = async (e) => {
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      setArticle(newArticle);
+      console.log(newArticle);
+    }
+  };
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/*  search*/}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className=" relative flex justify-center items-center"
+          onSubmit={handleSumbit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a url"
+            value={article.url}
+            onChange={(e) =>
+              setArticle({
+                ...article,
+                url: e.target.value,
+              })
+            }
+            required
+            className="url_input peer"
+          />
+          <button type="submit" className="submit_btn">
+            ➤
+          </button>
+        </form>
+        {/* browse */}
+      </div>
+      {/* display result */}
+    </section>
+  );
+};
+export default Demo;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import { logo } from '../assets';
+import React from 'react';
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => window.open('https://github.com/uhrzel')}
+          className="black_btn"
+        >
+          github
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Article with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with summarize, an open-source article summarrizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
 
-export default Hero
+export default Hero;
