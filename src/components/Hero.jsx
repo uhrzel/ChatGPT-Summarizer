bad4d07a81e1925a01d2771ee99f73fb2e9e63f4
Arzel John Zolina <80521619+ArzelJrz10@users.noreply.github.com>
2023-08-28 23:22:06 +0800

    hayst

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,12 +14,11 @@ const Hero = () => {
         </button>
       </nav>
       <h1 className="head_text">
-        Summarize Article with <br className="max-md:hidden" />
+        Summarize Article with <br className='max-md:hidden' />
         <span className="orange_gradient">OpenAI GPT-4</span>
       </h1>
-      <h2 className="desc">
-        Simplify your reading with summarize, an open-source article summarrizer
-        that transforms lengthy articles into clear and concise summaries
+      <h2 className='desc'>
+        Simplify your reading with summarize, an open-source article summarrizer that transforms lengthy
       </h2>
     </header>
   );
--- src/services/article.js
+++ src/services/article.js
@@ -1,13 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+/* 
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY; */
 
 export const articleApi = createApi({
   reducerPath: 'articleApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', rapidApiKey);
+      headers.set(
+        'X-RapidAPI-Key',
+        '91c79cbfa6msh6ea3a1337107653p1f24a7jsn0b4fc372ed77',
+      );
       headers.set(
         'X-RapidAPI-Host',
         'article-extractor-and-summarizer.p.rapidapi.com',
@@ -20,7 +24,6 @@ export const articleApi = createApi({
       query: (params) =>
         `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
     }),
-    
   }),
 });
 export const { useLazyGetSummaryQuery } = articleApi;
